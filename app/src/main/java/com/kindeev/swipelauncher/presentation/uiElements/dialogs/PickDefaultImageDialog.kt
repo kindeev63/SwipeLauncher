29150564aa01a5cc317dcba648497ee5e7608e8d
Kindeev <kindeevaleksandr163@mail.ru>
2023-12-12 21:14:18 +0400

    upgrade PickDefaultImageDialog

--- app/src/main/java/com/kindeev/swipelauncher/presentation/uiElements/dialogs/PickDefaultImageDialog.kt
+++ app/src/main/java/com/kindeev/swipelauncher/presentation/uiElements/dialogs/PickDefaultImageDialog.kt
@@ -3,19 +3,31 @@ package com.kindeev.swipelauncher.presentation.uiElements.dialogs
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -23,37 +35,65 @@ import androidx.compose.ui.window.DialogProperties
 import com.kindeev.swipelauncher.data.DefaultImageWithName
 import com.kindeev.swipelauncher.data.DefaultImages
 
+
 @Composable
 fun PickDefaultImageDialog(
     pickedId: Int,
     onPick: (id: Int) -> Unit,
     onDismissRequest: () -> Unit
 ) {
-    val screenWidth = LocalConfiguration.current.screenWidthDp
+    var picked by remember {
+        mutableStateOf(pickedId)
+    }
+    val screenConfiguration = LocalConfiguration.current
     Dialog(
         onDismissRequest = onDismissRequest,
         properties = DialogProperties(usePlatformDefaultWidth = false)
     ) {
-        Box(
+        Column(
             modifier = Modifier
-                .width(screenWidth.dp - 20.dp)
-                .height((screenWidth.dp - 20.dp) * 3 / 2),
-            contentAlignment = Alignment.Center
+                .width(screenConfiguration.screenWidthDp.dp - 20.dp)
+                .heightIn(max = (screenConfiguration.screenHeightDp / 3 * 2).dp)
+                .wrapContentHeight()
+                .clip(RoundedCornerShape(20.dp))
+                .background(Color.White)
+                .padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             LazyVerticalGrid(
-                columns = GridCells.Adaptive((screenWidth / 3).dp)
+                columns = GridCells.Fixed(((screenConfiguration.screenWidthDp - 20) / 50).toInt())
             ) {
                 items(items = DefaultImages.images) { item: DefaultImageWithName ->
                     Image(
                         modifier = Modifier
                             .size(50.dp)
-                            .background(if (item.defaultImage.id == pickedId) Color.Gray.copy(alpha = 0.5f) else Color.Transparent)
-                            .clickable { onPick(item.defaultImage.id) },
+                            .background(if (item.defaultImage.id == picked) Color.Gray.copy(alpha = 0.5f) else Color.Transparent)
+                            .clickable { picked = item.defaultImage.id },
                         painter = painterResource(
                             id = item.defaultImage.id
                         ),
                         contentDescription = null
                     )
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                TextButton(
+                    onClick = {
+                        onDismissRequest()
+                    }
+                ) {
+                    Text(text = "Cancel")
+                }
+                TextButton(
+                    onClick = {
+                        onPick(picked)
+                    }
+                ) {
+                    Text(text = "Save")
                 }
             }
         }
