1a7964f798f9c8197c5202284af42264f9bd89ea
knomster <kindeevaleksandr163@mail.ru>
2026-08-17 12:52:15 +0300

    Change version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ configure<ApplicationExtension> {
         applicationId = "com.kindeev.swipelauncher"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
+        versionCode = 6
         versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
